6cf6a31568d8cde8d4b21f06d661feb0cc0a7414
Yasunori Sakada <jpeggogei@gmail.com>
2019-01-28 20:44:27 +0900

    課題を提出します。確認よろしくお願いいたします。

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'users/new'
+  get 'pages/help'
+  # get 'pages/index'
+  root 'pages#index'
+
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
